5d07cd78b254e7792be8a846f21030bba12dfedd
Cihan <cihangonen35@gmail.com>
2022-01-19 13:45:41 +0300

    code sent

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,3 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../../hooks/useAuthContext";
 export default function Home() {
-  return <div>home</div>;
+  const { user, dispatch } = useAuthContext();
+  const navigate = useNavigate();
+
+  const logout = async (e) => {
+    e.preventDefault();
+    try {
+      localStorage.removeItem("token");
+      dispatch({ type: "LOGOUT" });
+      navigate("/signin");
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+  return (
+    <div>
+      {user && (
+        <button className="btn" onClick={logout}>
+          Log out
+        </button>
+      )}
+    </div>
+  );
 }
--- server/routes/generalRoutes.js
+++ server/routes/generalRoutes.js
@@ -3,5 +3,6 @@ const generalController = require("../controllers/generalController");
 const authorization = require("../middlewares/authorization");
 
 router.post("/note", generalController.note_post);
+router.post("/sendVerifEmail", generalController.verifEmail_post);
 
 module.exports = router;
